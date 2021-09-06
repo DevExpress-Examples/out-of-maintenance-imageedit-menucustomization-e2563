b2d01a925bae91c54a603b3c1d57a7cbbc8b8438
DevExpressExampleBot <fakeEmail>
2021-09-06 19:25:17 +0300

    Source auto update [skip ci]

--- VB/CustomizationDemo/ShaderEffect.vb
+++ VB/CustomizationDemo/ShaderEffect.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Text
@@ -10,21 +9,14 @@ Imports System.Windows.Media
 Namespace CustomizationDemo
 	Public Class GrayscaleEffect
 		Inherits ShaderEffect
-		Private Shared fPixelShader As New PixelShader() With { _
-			.UriSource = New Uri("pack://application:,,,/CustomizationDemo;component/GrayscaleEffect.ps")}
-		Public Shared ReadOnly InputProperty As DependencyProperty = _
-			ShaderEffect.RegisterPixelShaderSamplerProperty("Input", GetType(GrayscaleEffect), 0)
-		Public Shared ReadOnly DesaturationFactorProperty As DependencyProperty = _
-			DependencyProperty.Register("DesaturationFactor", _
-										GetType(Double), _
-										GetType(GrayscaleEffect), _
-										New UIPropertyMetadata(0.0, _
-															   PixelShaderConstantCallback(0), _
-															   AddressOf CoerceDesaturationFactor))
-		Private Shared Function CoerceDesaturationFactor(ByVal d As DependencyObject, _
-				ByVal value As Object) As Object
+
+'INSTANT VB NOTE: The field pixelShader was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private Shared pixelShader_Conflict As New PixelShader() With {.UriSource = New Uri("pack://application:,,,/CustomizationDemo;component/GrayscaleEffect.ps")}
+		Public Shared ReadOnly InputProperty As DependencyProperty = ShaderEffect.RegisterPixelShaderSamplerProperty("Input", GetType(GrayscaleEffect), 0)
+		Public Shared ReadOnly DesaturationFactorProperty As DependencyProperty = DependencyProperty.Register("DesaturationFactor", GetType(Double), GetType(GrayscaleEffect), New UIPropertyMetadata(0.0, PixelShaderConstantCallback(0), AddressOf CoerceDesaturationFactor))
+		Private Shared Function CoerceDesaturationFactor(ByVal d As DependencyObject, ByVal value As Object) As Object
 			Dim effect As GrayscaleEffect = CType(d, GrayscaleEffect)
-			Dim newFactor As Double = CDbl(value)
+			Dim newFactor As Double = DirectCast(value, Double)
 			If newFactor < 0.0 OrElse newFactor > 1.0 Then
 				Return effect.DesaturationFactor
 			End If
@@ -32,13 +24,13 @@ Namespace CustomizationDemo
 		End Function
 
 		Public Sub New()
-			PixelShader = fPixelShader
+			PixelShader = pixelShader_Conflict
 			UpdateShaderValue(InputProperty)
 			UpdateShaderValue(DesaturationFactorProperty)
 		End Sub
 		Public Property Input() As Brush
 			Get
-				Return CType(GetValue(InputProperty), Brush)
+				Return DirectCast(GetValue(InputProperty), Brush)
 			End Get
 			Set(ByVal value As Brush)
 				SetValue(InputProperty, value)
@@ -46,7 +38,7 @@ Namespace CustomizationDemo
 		End Property
 		Public Property DesaturationFactor() As Double
 			Get
-				Return CDbl(GetValue(DesaturationFactorProperty))
+				Return DirectCast(GetValue(DesaturationFactorProperty), Double)
 			End Get
 			Set(ByVal value As Double)
 				SetValue(DesaturationFactorProperty, value)
