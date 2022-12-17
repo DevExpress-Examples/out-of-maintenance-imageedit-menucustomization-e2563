f7991ed4f877852ab5ab3eb150f6cc393528fa0a
DevExpressExampleBot <fakeEmail>
2022-12-17 15:08:30 +0400

    Source auto update [skip ci]

--- VB/CustomizationDemo/MainWindow.xaml.vb
+++ VB/CustomizationDemo/MainWindow.xaml.vb
@@ -1,37 +1,24 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
-Imports System.Windows.Media.Effects
 
 Namespace CustomizationDemo
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub OnApplyEffectButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			If edit.ImageEffect Is Nothing Then
-				edit.ImageEffect = New GrayscaleEffect()
-			End If
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub OnUndoEffectButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			edit.ImageEffect = Nothing
-		End Sub
-	End Class
+        Private Sub OnApplyEffectButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            If Me.edit.ImageEffect Is Nothing Then Me.edit.ImageEffect = New GrayscaleEffect()
+        End Sub
+
+        Private Sub OnUndoEffectButtonClick(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.edit.ImageEffect = Nothing
+        End Sub
+    End Class
 End Namespace
--- VB/CustomizationDemo/ShaderEffect.vb
+++ VB/CustomizationDemo/ShaderEffect.vb
@@ -1,48 +1,50 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
+Imports System
 Imports System.Windows.Media.Effects
 Imports System.Windows
 Imports System.Windows.Media
 
 Namespace CustomizationDemo
-	Public Class GrayscaleEffect
-		Inherits ShaderEffect
 
-'INSTANT VB NOTE: The field pixelShader was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private Shared pixelShader_Conflict As New PixelShader() With {.UriSource = New Uri("pack://application:,,,/CustomizationDemo;component/GrayscaleEffect.ps")}
-		Public Shared ReadOnly InputProperty As DependencyProperty = ShaderEffect.RegisterPixelShaderSamplerProperty("Input", GetType(GrayscaleEffect), 0)
-		Public Shared ReadOnly DesaturationFactorProperty As DependencyProperty = DependencyProperty.Register("DesaturationFactor", GetType(Double), GetType(GrayscaleEffect), New UIPropertyMetadata(0.0, PixelShaderConstantCallback(0), AddressOf CoerceDesaturationFactor))
-		Private Shared Function CoerceDesaturationFactor(ByVal d As DependencyObject, ByVal value As Object) As Object
-			Dim effect As GrayscaleEffect = CType(d, GrayscaleEffect)
-			Dim newFactor As Double = DirectCast(value, Double)
-			If newFactor < 0.0 OrElse newFactor > 1.0 Then
-				Return effect.DesaturationFactor
-			End If
-			Return newFactor
-		End Function
+    Public Class GrayscaleEffect
+        Inherits ShaderEffect
 
-		Public Sub New()
-			PixelShader = pixelShader_Conflict
-			UpdateShaderValue(InputProperty)
-			UpdateShaderValue(DesaturationFactorProperty)
-		End Sub
-		Public Property Input() As Brush
-			Get
-				Return DirectCast(GetValue(InputProperty), Brush)
-			End Get
-			Set(ByVal value As Brush)
-				SetValue(InputProperty, value)
-			End Set
-		End Property
-		Public Property DesaturationFactor() As Double
-			Get
-				Return DirectCast(GetValue(DesaturationFactorProperty), Double)
-			End Get
-			Set(ByVal value As Double)
-				SetValue(DesaturationFactorProperty, value)
-			End Set
-		End Property
-	End Class
+        Private Shared pixelShader As PixelShader = New PixelShader() With {.UriSource = New Uri("pack://application:,,,/CustomizationDemo;component/GrayscaleEffect.ps")}
+
+        Public Shared ReadOnly InputProperty As DependencyProperty = RegisterPixelShaderSamplerProperty("Input", GetType(GrayscaleEffect), 0)
+
+        Public Shared ReadOnly DesaturationFactorProperty As DependencyProperty = DependencyProperty.Register("DesaturationFactor", GetType(Double), GetType(GrayscaleEffect), New UIPropertyMetadata(0.0, PixelShaderConstantCallback(0), AddressOf CoerceDesaturationFactor))
+
+        Private Shared Function CoerceDesaturationFactor(ByVal d As DependencyObject, ByVal value As Object) As Object
+            Dim effect As GrayscaleEffect = CType(d, GrayscaleEffect)
+            Dim newFactor As Double = CDbl(value)
+            If newFactor < 0.0 OrElse newFactor > 1.0 Then Return effect.DesaturationFactor
+            Return newFactor
+        End Function
+
+        Public Sub New()
+            PixelShader = pixelShader
+            UpdateShaderValue(InputProperty)
+            UpdateShaderValue(DesaturationFactorProperty)
+        End Sub
+
+        Public Property Input As Brush
+            Get
+                Return CType(GetValue(InputProperty), Brush)
+            End Get
+
+            Set(ByVal value As Brush)
+                SetValue(InputProperty, value)
+            End Set
+        End Property
+
+        Public Property DesaturationFactor As Double
+            Get
+                Return CDbl(GetValue(DesaturationFactorProperty))
+            End Get
+
+            Set(ByVal value As Double)
+                SetValue(DesaturationFactorProperty, value)
+            End Set
+        End Property
+    End Class
 End Namespace
